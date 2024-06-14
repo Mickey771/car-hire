733129dd34f576a899c2ff38018186ef2f44fa47
Ozurmba chibuike <ocbmf17@gmail.com>
2024-06-14 21:49:48 +0100

    Add Google Translator Integration and Email Submission Functionality
    Google Translator Integration:
    
    Added Google Translator to the website to support language translation.
    Included CSS styling at the end of the main CSS file to style the translator elements.
    Created a new JavaScript file language.js to handle the functionality of the translator.
    
    Form Submission to Email:
    
    Implemented functionality for form submission to send an email with the selected car rental details.
    Created a new JavaScript file emailsender.js to manage email sending via EmailJS.
    Styled the form submission button to indicate submission status.
    
    HTML Edits:
    
    Made necessary edits to the HTML to incorporate the translator and form submission functionalities.

---
+++ js/language.js
@@ -0,0 +1,13 @@
+// console.log("done it well");
+
+
+
+  
+
+  function googleTranslateElementInit() {
+      new google.translate.TranslateElement(
+          {pageLanguage: 'gn'},
+          'google_translate_element'
+      );
+  } 
+
